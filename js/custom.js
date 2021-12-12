b7e22521bede105c1d85aea99ed72fe9bb15ae89
hh880617 <hh880617@gmail.com>
2021-12-12 17:29:16 +0900

    포폴 등록

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function(){
+  // mobile gnb menu
+  $('.header__trigger').click(function(){
+    $(this).toggleClass('active')
+    $('.header__gnb').toggleClass('active')
+  })
+  $('section, .header__gnb__menu a').click(function(){
+    $('.header__gnb').removeClass('active')
+    $('.header__trigger').removeClass('active')
+  })
+  
+  //project slick slide
+  $('.project-photo').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear'
+  })
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# 웹포폴 프로젝트<br>
+1. 웹표준 홈페이지
+    - 시맨틱 태그
+    - 웹표준
+    - 웹접근성
+    - swiper slide libray
+    - gsap libray
+    - youtube api
+1. 반응형 홈페이지
+    - 시맨틱 태그
+    - 웹표준
+    - 웹접근성
+    - mediaquery
+    - bootstrap   
+    - 지도 api 
+1. 모바일 홈페이지
+    - jquery(object, array, animation)
